1790e0580c243372d489fa086e18fd4f69e0b831
Leonardo <leonardolp@gmail.com>
2021-03-29 18:05:07 -0300

    Adicionando validaçao das funçoes do validators.js

--- dashboard/.eslintrc.js
+++ dashboard/.eslintrc.js
@@ -12,13 +12,12 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   },
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        '**/*.spec.js'
       ],
       env: {
         jest: true

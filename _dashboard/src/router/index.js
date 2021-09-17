8531651d64e5894611ff437cce7410cd39ea3d70
Leonardo <leonardolp@gmail.com>
2021-09-17 14:40:11 -0300

    Teste

---
+++ _dashboard/src/router/index.js
@@ -0,0 +1,42 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const Home = () => import('../views/Home')
+const Feedbacks = () => import('../views/Feedbacks')
+const Credencials = () => import('../views/Credencials')
+
+export const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/feedbacks',
+    name: 'Feedbacks',
+    component: Feedbacks,
+    meta: {
+      hasAuth: true
+    }
+  },
+  {
+    path: '/credencials',
+    name: 'Credencials',
+    component: Credencials,
+    meta: {
+      hasAuth: true
+    }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: {
+      name: 'Home'
+    }
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory('/'),
+  routes
+})
+
+export default router
